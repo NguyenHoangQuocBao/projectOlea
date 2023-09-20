a32528846032cdede271385b0e362174ad7ea94b
cnvs_quoc_bao <hoangquocbao650@gmail.com>
2023-09-20 18:00:30 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+//  Initialize Swiper
+// letswiper = new Swiper(".mySwiper", {
+//     scrollbar: {
+//       el: ".swiper-scrollbar",
+//       hide: true,
+//     },
+//    });
+
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   });
